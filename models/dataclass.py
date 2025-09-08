8553b10690fb316fe2aa56d062c5ed008b23dcfe
NYXBAM <delirium.dev@proton.me>
2025-09-07 23:22:23 -0700

    Update dataclass.py

--- models/dataclass.py
+++ models/dataclass.py
@@ -33,6 +33,40 @@ class Transaction:
             'block_number': self.block_number,
             'block_hash': self.block_hash
         }
+
+'''  # if u have error, just remove slots=True, this feat work only python 3.10+, for older version. uncomment code below 
+from typing import Optional
+
+class Transaction:
+    __slots__ = ['blockchain', 'amount', 'amount_usd', 'id', 'classification', 
+                 'link', 'hash', 'from_a', 'to', 'value', 'block_number', 'block_hash']
     
+    def __init__(self, 
+                 blockchain: Optional[str] = None,
+                 amount: Optional[float] = None,
+                 amount_usd: Optional[float] = None,
+                 id: Optional[str] = None,
+                 classification: Optional[str] = None,
+                 link: Optional[str] = None,
+                 hash: Optional[str] = None,
+                 from_a: Optional[str] = None,
+                 to: Optional[str] = None,
+                 value: Optional[float] = None,
+                 block_number: Optional[int] = None,
+                 block_hash: Optional[str] = None):
+        self.blockchain = blockchain
+        self.amount = amount
+        self.amount_usd = amount_usd
+        self.id = id
+        self.classification = classification
+        self.link = link
+        self.hash = hash
+        self.from_a = from_a
+        self.to = to
+        self.value = value
+        self.block_number = block_number
+        self.block_hash = block_hash
+'''
+
     
     
